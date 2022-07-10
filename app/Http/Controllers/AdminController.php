f06f873f5fd1657fac199ba3f7e0140c813d5b1b
pourghadiriHossein <hossein.654321@yahoo.com>
2022-07-10 11:22:31 +0430

    session 5

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,300 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Address;
+use App\Models\Category;
+use App\Models\City;
+use App\Models\Comment;
+use App\Models\Contact;
+use App\Models\Discount;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Models\Region;
+use App\Models\Tag;
+use App\Models\Tool;
+use App\Models\Transaction;
+use App\Models\User;
+use App\Models\Zone;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AdminController extends Controller
+{
+    //index
+    public function adminIndex()
+    {
+        return view('admin.layout.adminLayout');
+    }
+    //user
+    public function adminVisitUser()
+    {
+        return view('admin.user.adminVisitUser');
+    }
+
+    public function adminAddUser()
+    {
+        return view('admin.user.adminAddUser');
+    }
+
+    public function adminPostAddUser(Request $request)
+    {
+        return redirect(route('adminVisitUser'));
+    }
+
+    public function adminUpdateUser($id)
+    {
+        return view('admin.user.adminUpdateUser');
+    }
+
+    public function adminPostUpdateUser()
+    {
+        return redirect(route('adminVisitUser'));
+    }
+    //permission
+    public function adminVisitPermission()
+    {
+        return view('admin.permission.adminVisitPermission');
+    }
+
+    public function adminAddPermission()
+    {
+        return view('admin.permission.adminAddPermission');
+    }
+
+    public function adminUpdatePermission()
+    {
+        return view('admin.permission.adminUpdatePermission');
+    }
+
+    public function adminPostAddPermission(Request $request)
+    {
+        return redirect(route('adminVisitPermission'));
+    }
+
+    public function adminPostUpdatePermission()
+    {
+        return redirect(route('adminVisitPermission'));
+    }
+
+    public function adminDeletePermission()
+    {
+        return redirect(route('adminVisitPermission'));
+    }
+    //role
+    public function adminVisitRole()
+    {
+        return view('admin.role.adminVisitRole');
+    }
+
+    public function adminAddRole()
+    {
+        return view('admin.role.adminAddRole');
+    }
+
+    public function adminUpdateRole($roleId)
+    {
+        return view('admin.role.adminUpdateRole');
+    }
+    public function adminPostAddRole()
+    {
+        return redirect(route('adminVisitRole'));
+    }
+
+    public function adminPostUpdateRole()
+    {
+        return redirect(route('adminVisitRole'));
+    }
+
+    public function adminDeleteRole()
+    {
+        return redirect(route('adminVisitRole'));
+    }
+    //category
+    public function adminVisitCategory()
+    {
+        return view('admin.category.adminVisitCategory');
+    }
+
+    public function adminAddCategory()
+    {
+        return view('admin.category.adminAddCategory');
+    }
+
+    public function adminAddParentCategory()
+    {
+        return view('admin.category.adminAddParentCategory');
+    }
+
+    public function adminUpdateCategory()
+    {
+        return view('admin.category.adminUpdateCategory');
+    }
+    public function adminPostAddCategory(Request $request)
+    {
+        return redirect(route('adminVisitCategory'));
+    }
+    public function adminPostAddParentCategory()
+    {
+        return redirect(route('adminVisitCategory'));
+    }
+    public function adminPostUpdateCategory()
+    {
+        return redirect(route('adminVisitCategory'));
+    }
+    //tag
+    public function adminVisitTag()
+    {
+        return view('admin.tag.adminVisitTag');
+    }
+
+    public function adminAddTag()
+    {
+        return view('admin.tag.adminAddTag');
+    }
+
+    public function adminUpdateTag()
+    {
+        return view('admin.tag.adminUpdateTag');
+    }
+    public function adminPostAddTag()
+    {
+        return redirect(route('adminVisitTag'));
+    }
+    public function adminPostUpdateTag()
+    {
+        return redirect(route('adminVisitTag'));
+    }
+    //discount
+    public function adminVisitDiscount()
+    {
+        return view('admin.discount.adminVisitDiscount');
+    }
+    public function adminAddDiscount()
+    {
+        return view('admin.discount.adminAddDiscount');
+    }
+    public function adminUpdateDiscount()
+    {
+        return view('admin.discount.adminUpdateDiscount');
+    }
+    public function adminPostAddDiscount()
+    {
+        return redirect(route('adminVisitDiscount'));
+    }
+    public function adminPostUpdateDiscount()
+    {
+        return redirect(route('adminVisitDiscount'));
+    }
+    //product
+    public function adminVisitProduct()
+    {
+        return view('admin.product.adminVisitProduct');
+    }
+    public function adminAddProduct()
+    {
+        return view('admin.product.adminAddProduct');
+    }
+    public function adminUpdateProduct()
+    {
+        return view('admin.product.adminUpdateProduct');
+    }
+    public function adminPostAddProduct()
+    {
+        return redirect(route('adminVisitProduct'));
+    }
+    public function adminPostUpdateProduct()
+    {
+        return redirect(route('adminVisitProduct'));
+    }
+
+    public function adminDeleteProductImage($imageID)
+    {
+        return redirect(route('adminVisitProduct'));
+    }
+    //comment
+    public function adminVisitComment()
+    {
+        return view('admin.comment.adminVisitComment');
+    }
+    public function adminUpdateComment()
+    {
+        return view('admin.comment.adminUpdateComment');
+    }
+    public function adminPostUpdateComment()
+    {
+        return redirect(route('adminVisitComment'));
+    }
+    //region & city & zone
+    //region
+    public function adminVisitRegion()
+    {
+        return view('admin.RCZ.adminVisitRegion');
+    }
+    public function adminAddRegion()
+    {
+        return view('admin.RCZ.adminAddRegion');
+    }
+    public function adminUpdateRegion()
+    {
+        return view('admin.RCZ.adminUpdateRegion');
+    }
+    public function adminPostAddRegion()
+    {
+        return redirect(route('adminVisitRegion'));
+    }
+    public function adminPostUpdateRegion()
+    {
+        return redirect(route('adminVisitRegion'));
+    }
+    //city
+    public function adminVisitCity()
+    {
+        return view('admin.RCZ.adminVisitCity');
+    }
+    public function adminAddCity($regionID)
+    {
+        return view('admin.RCZ.adminAddCity',compact('regionID'));
+    }
+    public function adminUpdateCity($cityID)
+    {
+        return view('admin.RCZ.adminUpdateCity');
+    }
+    public function adminPostAddCity()
+    {
+        return redirect(route('adminVisitCity'));
+    }
+    public function adminPostUpdateCity()
+    {
+        return redirect(route('adminVisitCity'));
+    }
+    //address
+    public function adminVisitAddress()
+    {
+        return view('admin.address.adminVisitAddress');
+    }
+    public function adminDeleteAddress($addressID)
+    {
+        return redirect(route('adminVisitAddress'));
+    }
+    //order
+    public function adminVisitOrder()
+    {
+        return view('admin.order.adminVisitOrder');
+    }
+    //transaction
+    public function adminVisitTransaction()
+    {
+        return view('admin.transaction.adminVisitTransaction');
+    }
+    //contact
+    public function adminVisitContact()
+    {
+        return view('admin.contact.adminVisitContact');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CheckController;
@@ -21,3 +22,76 @@ Route::get('showCategory',[CategoryController::class,'showCategory'])->name('sho
 Route::get('showProduct',[ProductController::class,'showProduct'])->name('showProduct');
 Route::get('login',[UserController::class,'login'])->name('login');
 Route::get('register',[UserController::class,'register'])->name('register');
+
+Route::group(['prefix' => 'admin'], function (){
+    Route::get('index',[AdminController::class, 'adminIndex'])->name('adminIndex');
+    Route::get('visitUser',[AdminController::class, 'adminVisitUser'])->name('adminVisitUser');
+    Route::get('addUser',[AdminController::class, 'adminAddUser'])->name('adminAddUser');
+    Route::get('updateUser/{id}',[AdminController::class, 'adminUpdateUser'])->name('adminUpdateUser');
+    Route::post('addUser',[AdminController::class, 'adminPostAddUser'])->name('adminPostAddUser');
+    Route::post('updateUser/{id}',[AdminController::class, 'adminPostUpdateUser'])->name('adminPostUpdateUser');
+//role & permission
+    Route::get('visitPermission',[AdminController::class, 'adminVisitPermission'])->name('adminVisitPermission');
+    Route::get('addPermission',[AdminController::class, 'adminAddPermission'])->name('adminAddPermission');
+    Route::get('updatePermission/{id}',[AdminController::class, 'adminUpdatePermission'])->name('adminUpdatePermission');
+    Route::get('visitRole',[AdminController::class, 'adminVisitRole'])->name('adminVisitRole');
+    Route::get('addRole',[AdminController::class, 'adminAddRole'])->name('adminAddRole');
+    Route::get('updateRole/{id}',[AdminController::class, 'adminUpdateRole'])->name('adminUpdateRole');
+    Route::post('addPermission',[AdminController::class, 'adminPostAddPermission'])->name('adminPostAddPermission');
+    Route::post('updatePermission/{id}',[AdminController::class, 'adminPostUpdatePermission'])->name('adminPostUpdatePermission');
+    Route::get('deletePermission/{id}',[AdminController::class, 'adminDeletePermission'])->name('adminDeletePermission');
+    Route::post('addRole',[AdminController::class, 'adminPostAddRole'])->name('adminPostAddRole');
+    Route::post('updateRole/{id}',[AdminController::class, 'adminPostUpdateRole'])->name('adminPostUpdateRole');
+    Route::get('deleteRole/{id}',[AdminController::class, 'adminDeleteRole'])->name('adminDeleteRole');
+//category
+    Route::get('visitCategory',[AdminController::class, 'adminVisitCategory'])->name('adminVisitCategory');
+    Route::get('addCategory',[AdminController::class, 'adminAddCategory'])->name('adminAddCategory');
+    Route::get('addParentCategory/{id}',[AdminController::class, 'adminAddParentCategory'])->name('adminAddParentCategory');
+    Route::get('updateCategory/{id}',[AdminController::class, 'adminUpdateCategory'])->name('adminUpdateCategory');
+    Route::post('addCategory',[AdminController::class, 'adminPostAddCategory'])->name('adminPostAddCategory');
+    Route::post('addParentCategory/{id}',[AdminController::class, 'adminPostAddParentCategory'])->name('adminPostAddParentCategory');
+    Route::post('updateCategory/{id}', [AdminController::class, 'adminPostUpdateCategory'])->name('adminPostUpdateCategory');
+//tag
+    Route::get('visitTag',[AdminController::class, 'adminVisitTag'])->name('adminVisitTag');
+    Route::get('addTag',[AdminController::class, 'adminAddTag'])->name('adminAddTag');
+    Route::get('updateTag/{id}',[AdminController::class, 'adminUpdateTag'])->name('adminUpdateTag');
+    Route::post('addTag', [AdminController::class, 'adminPostAddTag'])->name('adminPostAddTag');
+    Route::post('updateTag/{id}',[AdminController::class, 'adminPostUpdateTag'])->name('adminPostUpdateTag');
+//discount
+    Route::get('visitDiscount',[AdminController::class, 'adminVisitDiscount'])->name('adminVisitDiscount');
+    Route::get('addDiscount',[AdminController::class, 'adminAddDiscount'])->name('adminAddDiscount');
+    Route::get('updateDiscount/{id}',[AdminController::class, 'adminUpdateDiscount'])->name('adminUpdateDiscount');
+    Route::post('addDiscount', [AdminController::class, 'AdminPostAddDiscount'])->name('adminPostAddDiscount');
+    Route::post('updateDiscount/{id}', [AdminController::class, 'adminPostUpdateDiscount'])->name('adminPostUpdateDiscount');
+//product
+    Route::get('visitProduct', [AdminController::class, 'adminVisitProduct'])->name('adminVisitProduct');
+    Route::get('addProduct', [AdminController::class, 'adminAddProduct'])->name('adminAddProduct');
+    Route::get('updateProduct/{id}', [AdminController::class, 'adminUpdateProduct'])->name('adminUpdateProduct');
+    Route::post('addProduct',[AdminController::class, 'adminPostAddProduct'])->name('adminPostAddProduct');
+    Route::post('updateProduct/{id}',[AdminController::class, 'adminPostUpdateProduct'])->name('adminPostUpdateProduct');
+    Route::get('deleteProductImage/{id}',[AdminController::class, 'adminDeleteProductImage'])->name('adminDeleteProductImage');
+//comment
+    Route::get('visitComment', [AdminController::class, 'adminVisitComment'])->name('adminVisitComment');
+    Route::get('updateComment/{id}', [AdminController::class, 'adminUpdateComment'])->name('adminUpdateComment');
+    Route::post('updateComment/{id}',[AdminController::class, 'adminPostUpdateComment'])->name('adminPostUpdateComment');
+//region & city & zone
+    Route::get('visitRegion',[AdminController::class, 'adminVisitRegion'])->name('adminVisitRegion');
+    Route::get('addRegion',[AdminController::class, 'adminAddRegion'])->name('adminAddRegion');
+    Route::get('updateRegion/{id}',[AdminController::class, 'adminUpdateRegion'])->name('adminUpdateRegion');
+    Route::get('visitCity',[AdminController::class, 'adminVisitCity'])->name('adminVisitCity');
+    Route::get('addCity/{id}',[AdminController::class, 'adminAddCity'])->name('adminAddCity');
+    Route::get('updateCity/{id}',[AdminController::class, 'adminUpdateCity'])->name('adminUpdateCity');
+    Route::post('addRegion',[AdminController::class, 'adminPostAddRegion'])->name('adminPostAddRegion');
+    Route::post('updateRegion/{id}',[AdminController::class, 'adminPostUpdateRegion'])->name('adminPostUpdateRegion');
+    Route::post('addCity/{id}',[AdminController::class,'adminPostAddCity'])->name('adminPostAddCity');
+    Route::post('updateCity/{id}',[AdminController::class,'adminPostUpdateCity'])->name('adminPostUpdateCity');
+//address
+    Route::get('visitAddress',[AdminController::class, 'adminVisitAddress'])->name('adminVisitAddress');
+    Route::get('deleteAddress/{id}',[AdminController::class,'adminDeleteAddress'])->name('adminDeleteAddress');
+//order
+    Route::get('visitOrder',[AdminController::class, 'adminVisitOrder'])->name('adminVisitOrder');
+//transaction
+    Route::get('visitTransaction', [AdminController::class, 'adminVisitTransaction'])->name('adminVisitTransaction');
+//contact
+    Route::get('visitContact', [AdminController::class, 'adminVisitContact'])->name('adminVisitContact');
+});
